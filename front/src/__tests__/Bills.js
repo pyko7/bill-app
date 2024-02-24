033a24af33124bed3959d113a2a449f6a15ecf4e
julien henry <julienhenry967@gmail.com>
2024-02-24 14:03:11 +0100

    test: test handleClickEyeIcon for bills

--- front/src/__tests__/Bills.js
+++ front/src/__tests__/Bills.js
@@ -9,6 +9,8 @@ import store from "../__mocks__/store.js";
 
 import router from "../app/Router.js";
 import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
+import BillsUI from "../views/BillsUI.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -41,5 +43,26 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = dates.sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
+  })
+  describe("When I click on the eye icon", () => {
+    test("Then a modal should open", async () => {
+      const BillClass = new Bills({
+        document,
+        onNavigate,
+        store,
+        localStorage: null
+      });
+      const bills = await BillClass.getBills();
+
+      document.body.innerHTML = BillsUI({data: bills, loading:null, error:null})
+
+      const eyeIcon = screen.getAllByTestId('icon-eye')
+      const handleClickIconEye = jest.fn(() => BillClass.handleClickIconEye(eyeIcon[0]))
+      eyeIcon[0].addEventListener('click', handleClickIconEye)
+      userEvent.click(eyeIcon[0])
+      expect(handleClickIconEye).toHaveBeenCalled()
+      const modal = screen.getByTestId('modale-file')
+      expect(modal).toBeTruthy()
+    })
   })
 })
